1ec52b745f60e64ae6f6946010b3ad8c9ba73873
Michael Schoderer <michael@dev-app.de>
2023-12-02 10:19:45 +0100

    Day2 dirty

--- day-template/benches/bench.rs
+++ day-template/benches/bench.rs
@@ -1,16 +1,16 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use day02::*;
+use {{project-name}}::*;
 use utils::Part;
 
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let input = include_str!("../inputs/day02.txt");
+    let input = include_str!("../inputs/{{project-name}}.txt");
 
-    c.bench_function("day_02_part1", |b| b.iter(|| {
+    c.bench_function("{{project-name}}_part1", |b| b.iter(|| {
         let mut part = Part1;
         part.run_part(black_box(input))
     }));
-    c.bench_function("day_02_part2", |b| b.iter(|| {
+    c.bench_function("{{project-name}}_part2", |b| b.iter(|| {
         let mut part = Part2;
         part.run_part(black_box(input))
     }));
