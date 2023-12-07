c934fc8039a8fad1fd3a67d7cd78a4735d71ef96
Michael Schoderer <michael@dev-app.de>
2023-12-07 19:03:05 +0100

    Day 7

---
+++ days/day7/src/part1.rs
@@ -0,0 +1,42 @@
+pub struct Part1;
+
+impl utils::Part for Part1 {
+    type Intermediate = ();
+
+    type Output = u64;
+
+    fn map(&mut self, _input: &str) -> Self::Intermediate {
+        todo!()
+    }
+
+    fn reduce(&mut self, _input: Vec<Self::Intermediate>) -> Self::Output {
+        todo!()
+    }
+}
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn sample_input(){
+        use utils::Part;
+        let input = r#"
+        "#;
+
+        let mut part = super::Part1;
+
+        let res = part.run_part(input);
+
+        assert_eq!(res, 9999); //todo
+    }
+
+    #[test]
+    fn main() {
+        use utils::Part;
+        let input = include_str!("../inputs/day7.txt");
+
+        let mut part = super::Part1;
+
+        let result = part.run_part(input);
+        assert_eq!(9999, result); //todo
+    }
+}
